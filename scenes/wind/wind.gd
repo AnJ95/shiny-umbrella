c3c67ea700c56a812057067d10128e5c341a2ddf
LarryD <lirandattner@googlemail.com>
2024-12-21 13:30:26 +0100

    Added Wind Polygon
    Wind polygon for in editor improvements

--- scenes/wind/wind.gd
+++ scenes/wind/wind.gd
@@ -3,7 +3,7 @@ extends Node2D
 class_name Wind
 @export var size =Vector2i(20,20)
 @export_range(0, 360, 1, "degrees") var angle = 0.0
-@export var strength : float
+@export var strength: float = 5 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
