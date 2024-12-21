1f297def57d1faf9ab912228569d026b07856916
johfm <joimnetz@googlemail.com>
2024-12-21 17:07:47 +0100

    raindrop sound is now only playing when intended

--- scenes/raindrop/raindrop.gd
+++ scenes/raindrop/raindrop.gd
@@ -6,7 +6,6 @@ var velocity = Vector2(0,1)
 
 func _ready() -> void:
 	$Timer.wait_time=RAINDROP_LIFETIME
-	pass
 
 func _process(delta: float) -> void:
 	position += velocity
@@ -25,7 +24,5 @@ func _on_area_entered(area: Area2D) -> void:
 func _on_drop_sound_finished() -> void:
 	queue_free()
 
-
 func _on_timer_timeout():
 	queue_free()
-	pass # Replace with function body.
