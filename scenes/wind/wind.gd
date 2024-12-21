cd8c667ce8e00764c7910613587114cb4f28c889
LarryD <lirandattner@googlemail.com>
2024-12-21 14:25:12 +0100

    Changes to rain and wind debug visuals, Added rain and wind polygons

--- scenes/wind/wind.gd
+++ scenes/wind/wind.gd
@@ -3,10 +3,11 @@ extends Node2D
 class_name Wind
 @export var size =Vector2i(20,20)
 @export_range(0, 360, 1, "degrees") var angle = 0.0
-@export var strength: float = 5 
+@export var strength : float = 5 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	update_values()
 	pass # Replace with function body.
 
 func get_wind():
@@ -14,13 +15,8 @@ func get_wind():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	$WindArea/CollisionShape2D.shape.size = size
-	$WindArea.position.x = size.x/2.0
-	$WindArea/DebugHelper.global_rotation_degrees = angle
-	$DebugSprite.texture.size.x = size.x
-	$DebugSprite.texture.size.y = size.y
-	$DebugSprite.position.x = size.x/2
-
+	if Engine.is_editor_hint():
+		update_values()
 	
 class WindProperties:
 	var angle
@@ -29,3 +25,9 @@ class WindProperties:
 	func _init(angle, strength):
 		self.angle = angle
 		self.strength = strength
+
+func update_values():
+	$WindArea/CollisionShape2D.shape.size = size
+	$WindArea.position.x = size.x/2.0
+	$WindArea/DebugHelper.global_rotation_degrees = angle
+	$WindArea/DebugHelper.scale = Vector2((size.x+size.y/2.0)/10.0,(size.x+size.y/2.0)/10.0)
