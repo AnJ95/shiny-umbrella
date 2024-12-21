6caf1cc2081f43dcce69234516819be2e7e6b7cf
LarryD <lirandattner@googlemail.com>
2024-12-21 12:48:36 +0100

    Player movement rewrite
    Added consts and different control factors to player movement depending on state

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,49 +1,121 @@
 extends CharacterBody2D
 
+#Max speed of player, caps length of velocity each frame
+const MAX_SPEED = SPEED*2.0
+#Movement speed of player (maximum grounded speed)
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-#Safe area from rain
-const rainProtectionAngle = 30.0
+
+
+#MOVEMENT CONTROL CONSTS (grounded control factor = 1)
+#Movement control while in air
+const AIR_CONTROL = .8
+#Movement control while in wind
+const WIND_AIR_CONTROL = .6
+#Factor for movement, altered if in air or in wind
+var control_factor = 1.0
+
+#JUMP VARIABLES
+#Velocity applied on jump
+const JUMP_VELOCITY = -300.0
+#Jump startup time, can hold space to rise further
+const JUMP_TIME = 0.2
+#Current jump duration
+var jump_timer = 0.0
+
+#Time able to jump after leaving platform in delta
+const COYOTE_TIME = 0.2
+#Timer for counting time since leaving platform
+var coyote_timer = 0.0
+
+#INERTIA FACTORS - Fraction of Second (delta) to reach max speed or stop depending on control, ground, air
+#Fraction of second that movement needs to be applied to reach full speed
+const MOVEMENT_INERTIA_FACTOR = 10.0
+#Fraction of second to stop on ground
+const GROUND_INERTIA_FACTOR = 5.0
+#Fraction of second to stop in air (if SPEED is reached)
+const AIR_INERTIA_FACTOR = 1.0
+
+#BOOLS
+#Currently affected by wind
+var in_wind = false
+#Currently jumping
+var jumping = false
+
+var acceleration = Vector2(0.0,0.0)
+
+#Damping of wind force each frame
+const WIND_DAMPING = 0.2
+#Wind force applied each frame (added to velocity)
+var wind_force = Vector2(0.0,0.0)
+
+#Angle of protection of umbrella in degrees
+const UMBRELLA_PROTECTION_ANGLE = 30.0
 var hp = 100.0
 var umbrella_direction = Vector2(0.0,1.0)
 #Umbrella Angle in degrees
 var umbrellaAngle = 0.0
-var acceleration = Vector2(0.0,0.0)
-var damping = 0.5
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
+	control_factor = 1.0
 	if not is_on_floor():
+		control_factor = AIR_CONTROL
+		if in_wind:
+			control_factor = WIND_AIR_CONTROL
+		coyote_timer += delta
 		velocity += get_gravity() * delta
-
+	else:
+		jumping = false
+		coyote_timer = 0.0
+	if jumping == true:
+		jump_timer += delta
+	else:
+		jump_timer = 0.0
 	# Handle jump.
-	if Input.is_action_just_pressed("up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
+	if Input.is_action_just_pressed("up"):
+		if (is_on_floor() or coyote_timer <= COYOTE_TIME):
+			jumping = true
+			velocity.y = JUMP_VELOCITY
+	if Input.is_action_pressed("up"):
+		if(jumping == true and jump_timer <= JUMP_TIME):
+			velocity.y -= get_gravity().y/2.0*delta
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("left", "right")
 	if direction:
-		velocity.x = direction * SPEED
+		velocity.x = move_toward(velocity.x,direction * SPEED * control_factor, SPEED * control_factor*delta*MOVEMENT_INERTIA_FACTOR)
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		if is_on_floor():
+			velocity.x = move_toward(velocity.x, 0, SPEED*GROUND_INERTIA_FACTOR*delta)
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED*AIR_INERTIA_FACTOR*delta)
 
 	var winds = $umbrella/windable.get_wind_properties()
+	#If not in wind, wind_force decays by damping factor each second
+	if(winds.size() == 0): 
+		wind_force = wind_force * (1.0-WIND_DAMPING)
+		in_wind = false
+	else:
+		wind_force = Vector2(0.0,0.0)
+		in_wind = true
 	for wind in winds:
 		var wind_direction = Vector2.from_angle(deg_to_rad(wind.angle))
 		if(umbrella_direction.dot(wind_direction)>=0.4):
 			print("in wind")
-			acceleration = wind_direction*wind.strength
+			wind_force += wind_direction*wind.strength
 		else:
-			acceleration = wind_direction*wind.strength/2.0
-	velocity += acceleration
+			acceleration = 0.0
+	velocity += wind_force*SPEED*delta
+	if(velocity.length()>=MAX_SPEED):
+		velocity = velocity.normalized()*MAX_SPEED
 	move_and_slide()
-	acceleration = acceleration * damping
+
+	
 
 func applyRain(rainAngle):
 	var angleDiff = wrapf(rainAngle - umbrellaAngle,0.0,360.0)
 	#if protected from rain
-	if(angleDiff >= 90-rainProtectionAngle && angleDiff <= 90+rainProtectionAngle ):
+	if(angleDiff >= 90-UMBRELLA_PROTECTION_ANGLE && angleDiff <= 90+UMBRELLA_PROTECTION_ANGLE ):
 		pass
 	#not protected from rain
 	else:
