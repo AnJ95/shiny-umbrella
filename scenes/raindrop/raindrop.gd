c465b9c8b826621bd5da38939f87a2b76be52ea4
johfm <joimnetz@googlemail.com>
2024-12-21 16:34:12 +0100

    raindrop sound

--- scenes/raindrop/raindrop.gd
+++ scenes/raindrop/raindrop.gd
@@ -9,8 +9,15 @@ func _process(delta: float) -> void:
 	position += velocity
 
 func _on_body_entered(body: Node2D) -> void:
+	set_deferred("monitoring", false)
+	self.visible = false
 	body.hit_by_rain()
-	queue_free()
+	$drop_sound.play()
 
 func _on_area_entered(area: Area2D) -> void:
+	set_deferred("monitoring", false)
+	self.visible = false
+	$drop_sound.play()
+
+func _on_drop_sound_finished() -> void:
 	queue_free()
