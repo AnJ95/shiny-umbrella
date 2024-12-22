c40e4ff58065cc6045748b480370b0a1c8437b23
johfm <joimnetz@googlemail.com>
2024-12-22 14:09:56 +0100

    menu is now working

---
+++ scenes/level_select/level_buttons.gd
@@ -0,0 +1,13 @@
+extends VBoxContainer
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	
+	# check buttons
+	for i in range(get_children().size()):
+		if get_children()[i].button_pressed:
+			get_tree().change_scene_to_file(["res://scenes/levels/level_00.tscn",
+											"res://scenes/levels/level_liran.tscn",
+											"res://scenes/levels/level.tscn"][i])
