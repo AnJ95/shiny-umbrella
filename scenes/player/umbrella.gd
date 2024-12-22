f1828dcdec1b2ba92ecb94b42b6fcbe3c190c9ff
Angelo Aracri <angeloaracri@yahoo.de>
2024-12-22 01:54:16 +0100

    Next iteration of level

--- scenes/player/umbrella.gd
+++ scenes/player/umbrella.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+const MIN_VELOCITY = 400
+
 func _ready() -> void:
 	pass
 
@@ -9,6 +11,5 @@ func _process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	var relative_angle = rad_to_deg((body.position - $"..".position).angle())
 	var angle_diff = self.rotation_degrees - relative_angle
-	if not $"..".umbrella_open and (-20 < angle_diff and angle_diff < 20) and $"..".velocity_last_frame > 10:
+	if not $"..".umbrella_open and (-20 < angle_diff and angle_diff < 20) and $"..".velocity_last_frame > MIN_VELOCITY:
 		body.break_pudding()
-	print("rel: ", relative_angle, " diff: ", angle_diff)
